166ed6cb59dd7179ca27b1dae3b2ea3776cd6680
Thadeus Miseda <tmiseda@gmail.com>
2024-04-04 18:37:16 +0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
 
-export default async function Home() {
+export default function Home() {
   return (
-    <Button size="lg" variant="outline">
-      Click Me
-    </Button>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-600">
+     <div className="space-y-6">
+      <h1 className="text-6xl font-semibold text-white drop-shadow-md"> 🔒 Auth</h1>
+     </div>
+    </main>
   );
 }
