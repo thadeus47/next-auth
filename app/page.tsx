383f98a5829079cbef3bf89701fcbbf8ca7aca99
Thadeus Miseda <tmiseda@gmail.com>
2024-04-04 23:24:35 +0300

    landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
 import { Button } from "@/components/ui/button";
 
+const font = Poppins({
+  subsets: ["latin"],
+  weight:["600"]
+})
 export default function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-600">
-     <div className="space-y-6">
-      <h1 className="text-6xl font-semibold text-white drop-shadow-md"> 🔒 Auth</h1>
+     <div className="space-y-6 text-center">
+      <h1 className={cn(
+        "text-6xl font-semibold text-white drop-shadow-md")}> 🔒 Auth</h1>
+      <p className="text-white text-lg">
+        A simple authentication service
+      </p>
      </div>
     </main>
   );
