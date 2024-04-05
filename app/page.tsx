408538f3de584f52937aa2424ab46ab9377b99cd
Thadeus Miseda <tmiseda@gmail.com>
2024-04-05 22:50:11 +0300

    adding sign in button

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,17 @@ export default function Home() {
     <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-600">
      <div className="space-y-6 text-center">
       <h1 className={cn(
-        "text-6xl font-semibold text-white drop-shadow-md")}> 🔒 Auth</h1>
+        "text-6xl font-semibold text-white drop-shadow-md",
+        font.className,
+        )}> 🔒 Auth</h1>
       <p className="text-white text-lg">
         A simple authentication service
       </p>
+      <div className="div">
+        <Button variant="secondary" size="lg">
+          Sign In
+        </Button>
+      </div>
      </div>
     </main>
   );
