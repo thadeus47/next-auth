8b4ba83b405b1072bd68884eb41f3c6df281d7c8
Thadeus Miseda <tmiseda@gmail.com>
2024-04-09 00:10:14 +0300

    adding card component

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,7 +1,13 @@
+import { CardWrapper } from "./card-wrapper"
 export const LoginForm = () => {
     return(
-        <div>
+        <CardWrapper
+          headerLabel="Welcome Back"
+          backButtonLabel="Don't have an account?"
+          backButtonHref="/auth/register"
+          showSocial
+        >
             Login Form!
-        </div>
+        </CardWrapper>
     )
 }
