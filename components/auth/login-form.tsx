63b53f4de3987ce810880fa44224a05450654653
Thadeus Miseda <tmiseda@gmail.com>
2024-04-12 23:58:54 +0300

    minor change

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import * as z from "zod";
 
@@ -6,6 +6,16 @@ import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 
 import { LoginSchema } from "@/schemas";
+
+import { Input } from "@/components/ui/input";
+
+import { 
+    Form, 
+    FormControl, 
+    FormField,
+    FormItem, 
+    FormLabel, 
+    FormMessage } from "@/components/ui/form";
 
 
 import { CardWrapper } from "@/components/auth/card-wrapper"
@@ -15,8 +25,8 @@ export const LoginForm = () => {
         defaultValues: {
             email:"",
             password:"",
-        }
-    })
+        },
+    });
     return(
         <CardWrapper
           headerLabel="Welcome Back"
@@ -24,7 +34,28 @@ export const LoginForm = () => {
           backButtonHref="/auth/register"
           showSocial
         >
-            Login Form!
+           <Form {...form}>
+             <form 
+               onSubmit={form.handleSubmit(() => {})}
+               className="space-y-6"
+             >
+              <div className="space-y-4">
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field}) => (
+
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                
+                            </FormControl>
+                        </FormItem>
+                    )}
+                 />
+              </div>
+             </form>
+           </Form>
         </CardWrapper>
     )
 }
