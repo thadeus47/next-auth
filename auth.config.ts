043b8c0841d9a9520c38fbd1d5a5fcfeea8783c8
m_thado <tmiseda@gmail.com>
2024-05-22 16:39:58 -0400

    middleware updates

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,10 @@
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
 import type { NextAuthConfig } from "next-auth"
- 
+import credentials from "next-auth/providers/credentials"
+
+import { LoginSchema } from "@/schemas"
+
+
 // Notice this is only an object, not a full Auth.js instance
 export default {
-  providers: [GitHub,Google],
+  providers: [],
 } satisfies NextAuthConfig
