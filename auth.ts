5b7839e5ba6edc59d0f16ef036a1ced944d77088
m_thado <tmiseda@gmail.com>
2024-05-23 07:55:49 -0400

    api routes update

--- auth.ts
+++ auth.ts
@@ -1,7 +1,18 @@
+
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
- 
-export const { handlers: { GET, POST}, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub,Google],
-})
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+import { db } from "@/lib/db";
+import authConfig from "@/auth.config"
+
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
