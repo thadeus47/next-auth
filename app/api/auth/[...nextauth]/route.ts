8acd25115a7eb243a3e52da2c3b778d0f72f3ec4
m_thado <tmiseda@gmail.com>
2024-05-23 07:48:48 -0400

    api routes updates

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,2 @@
+export { GET, POST } from "@/auth"
 
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter";
-
-import { db } from "@/lib/db";
-import authConfig from "@/auth.config"
-
-// export const { GET, POST } = handlers
-/**
- * Configures the NextAuth.js authentication middleware for the application.
- * This code sets up the handlers for the GET and POST routes, as well as the authentication configuration.
- * The Prisma adapter is used to interact with the database, and the session strategy is set to "jwt".
- * The additional authentication configuration is provided through the `authConfig` object.
- */
-export const {
-  handlers: { GET, POST },
-  auth,
-} = NextAuth({
-  adapter: PrismaAdapter(db),
-  session: { strategy: "jwt" },
-  ...authConfig,
-});
--- auth.config.ts
+++ auth.config.ts
@@ -1,10 +1,36 @@
+import bcrypt from 'bcrypt';
 import type { NextAuthConfig } from "next-auth"
-import credentials from "next-auth/providers/credentials"
+import Credentials from "next-auth/providers/credentials"
 
 import { LoginSchema } from "@/schemas"
+import { getUserByEmail } from "@/data/user";
 
 
 // Notice this is only an object, not a full Auth.js instance
 export default {
-  providers: [],
+  providers: [
+    Credentials({
+      /**
+       * Authorizes a user by validating their credentials.
+       *
+       * @param credentials - The user's credentials, including email and password.
+       * @returns The authenticated user if the credentials are valid, or `null` if they are invalid.
+       */
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
+        }
+
+        return null;
+      },
+    })
+  ],
 } satisfies NextAuthConfig
