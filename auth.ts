ecb75f4cfbfbd7cafb18eba5ecc90ea5d2971d62
m_thado <tmiseda@gmail.com>
2024-05-24 07:21:50 -0400

    adding user role to the session

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
+import { getUserById } from "@/data/user";
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config"
 
@@ -14,13 +15,30 @@ export const {
 } = NextAuth({
   callbacks: {
     async session({ token, session }) {
+      console.log({
+        sessionToken: token,
+      })
+
         if (token.sub && session.user) {
           session.user.id = token.sub;
+        }
+
+        if (token.role && session.user) {
+          session.user.role = token.role;
         }
       
       return session
     },
     async jwt({ token }) {
+
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
       return token;
     }
   },
