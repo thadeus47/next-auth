ada5f528d82a9af02955eea8e624bf15930cbec4
m_thado <tmiseda@gmail.com>
2024-05-28 18:29:17 -0400

    adding auth providers update

--- auth.config.ts
+++ auth.config.ts
@@ -11,8 +11,11 @@ import { getUserByEmail } from "@/data/user";
 // Notice this is only an object, not a full Auth.js instance
 export default {
   providers: [
-    GitHub,
     Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       
