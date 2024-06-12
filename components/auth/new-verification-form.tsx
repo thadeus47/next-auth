d1ad5d37415631d2878fc652e39555c551cb0a54
m_thado <tmiseda@gmail.com>
2024-06-12 18:43:29 -0400

    verification server action updated

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,19 +1,37 @@
 'use client';
 
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import {  BeatLoader} from "react-spinners";
 import { useSearchParams } from "next/navigation";
 
+import { newVerification } from "@/actions/new-verification";
 import { CardWrapper } from "@/components/auth/card-wrapper";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
 
 export const NewVerificationForm = () => {
+
+    const [error, setError] = useState<string | undefined>();
+    const [success, setSuccess] = useState<string | undefined>();
 
     const searchParams = useSearchParams();
 
     const token = searchParams.get('token');
 
     const onSubmit = useCallback(() => {
-       console.log(token);
+        if (!token) {
+            setError("Misssing token");
+            return;
+        }
+
+       newVerification(token)
+          .then((data) => {
+            setSuccess(data.success);
+            setError(data.error)
+          })
+          .catch(() => {
+            setError("Something went wrong");
+          })
     }, [token]);
 
     useEffect(() => {
@@ -29,6 +47,8 @@ export const NewVerificationForm = () => {
            >
         <div className="flex items-center w-full justify-center">
            <BeatLoader />
+           <FormSuccess message={success} />
+           <FormError message={error} />
         </div>
         </CardWrapper>
     )
