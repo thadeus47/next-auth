c58575c28042ccc42f2e3e59cfd487868eb1b573
m_thado <tmiseda@gmail.com>
2024-06-12 18:30:31 -0400

    verification server action update

--- actions/new-verification.ts
+++ actions/new-verification.ts
@@ -29,5 +29,11 @@ export const newVerification = async (token: string) => {
             emailVerified: new Date(),
             email: existingToken.email
         }
-    })
+    });
+
+    await db.verificationToken.delete({
+        where: { id: existingToken.id }
+    });
+
+    return { success: "Email verified!" };
 }
