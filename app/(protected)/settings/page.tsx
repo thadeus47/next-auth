3f315b9974b6341841bfc5f88b52baf54b62f490
m_thado <tmiseda@gmail.com>
2024-07-18 14:01:11 -0400

    setting ui changes

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,22 +1,22 @@
 "use client"
 
 import { logout } from "@/actions/logout";
-import { useSession } from "next-auth/react";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
 
 
 const SettingsPage = () => {
-    const session =  useSession();
+    const user =  useCurrentUser();
 
     const onClick = () => {
         logout();
     }
      
     return (
-        <div>
-            {JSON.stringify(session)}
-                <button  onClick={onClick} type="submit">
-                    Sign out
-                </button>
+        <div className="bg-white p-10 rounded-xl">
+            <button  onClick={onClick} type="submit">
+                Sign out
+            </button>
         </div>
     )
 }
---
+++ components/auth/user-button.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import {
+    Avatar,
+    AvatarImage,
+    AvatarFallback,
+} from "@/components/ui/avatar";
+
+export const UserButton =() => {
+    return (
+        <div>
+            UserButton
+        </div>
+    )    
+}
