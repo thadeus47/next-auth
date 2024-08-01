f8745067a195700e3b69921b159ed39336724b69
m_thado <tmiseda@gmail.com>
2024-08-01 19:33:45 -0400

    settings page update

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,10 +1,21 @@
 "use client"
 
-import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import { useTransition } from "react";
 
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { settings } from "@/actions/settings";
 
 const SettingsPage = () => {
-    
+    const [isPending, startTransition] = useTransition();
+
+    const onClick = () => {
+        startTransition(() => {
+            settings({
+                name: "New Name"
+            });
+        });
+    }
      
     return (
         <Card className="w-[600px]">
@@ -13,6 +24,11 @@ const SettingsPage = () => {
                   ⚙️ Settings
                 </p>
             </CardHeader>
+            <CardContent>
+                <Button disabled={isPending} onClick={onClick}>
+                    Update name
+                </Button>
+            </CardContent>
         </Card>
     )
 }
