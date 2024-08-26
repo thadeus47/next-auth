d449e311324a07eae3afad3c0f4a25aacf6f26b2
m_thado <tmiseda@gmail.com>
2024-08-26 18:56:56 -0400

    settings changes minor

--- schemas/index.ts
+++ schemas/index.ts
@@ -9,7 +9,21 @@ export const SettingsSchema = z.object({
     email: z.optional(z.string().min(6)),
     password: z.optional(z.string().min(6)),
     newPassword: z.optional(z.string().min(6)),
-});
+})
+  .refine((data) => {
+    if (data.password &&  !data.newPassword){
+        return false;
+    }
+
+    if (data.newPassword && !data.password) {
+        return false;
+    }
+
+    return true
+  }, {
+      message: "New password is required!",
+      path: ["newPassword"],
+  })
 
 export const NewPasswordSchema = z.object({
     password: z.string().min(6, {
